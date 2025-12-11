f910cbf56674083a2f4d843f6fcbf4424f5685af
Juan <112003348+juanescan@users.noreply.github.com>
2025-12-10 20:43:44 -0500

    Update Food.js

--- src/models/Food.js
+++ src/models/Food.js
@@ -1,11 +1,23 @@
 const mongoose = require("mongoose");
 
 const foodSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  calorias: { type: Number, required: true },
-  ingredientes: [{ type: String }],
-  descripcion: { type: String },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+  tipo: { 
+    type: String, 
+    required: true,
+    enum: ["desayuno", "almuerzo", "cena", "otro"] 
+  },
+  calorias: { 
+    type: Number, 
+    required: true 
+  },
+  descripcion: { 
+    type: String 
+  },
+  userId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "User", 
+    required: true 
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Food", foodSchema);
